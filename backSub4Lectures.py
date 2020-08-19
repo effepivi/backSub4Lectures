eed4d23c23c092d4830aad5230a61b49001c3c8e
effepivi <franck.p.vidal@gmail.com>
2020-08-19 14:36:35 +0100

    Can choose background colour, can input ROI frm cmd line, and output ROI in terminal.

--- backSub4Lectures.py
+++ backSub4Lectures.py
@@ -26,8 +26,9 @@ def clickAndCrop(event, x, y, flags, param):
             update_ROI = True;
 
 
-parser = argparse.ArgumentParser(description='This program shows how to use background subtraction.')
-# parser.add_argument('--algo', type=str, help='Background subtraction method (KNN, MOG2).', default='MOG2')
+parser = argparse.ArgumentParser(description='Background subtraction to use with Chroma key.')
+parser.add_argument('--ROI', type=int, help='Corners of the region of interests.', nargs=4, required=False);
+parser.add_argument('--colour', type=int, help='Replace the backbround with this RGB colour (0 0 0 is black, 255 255 255 is white), useful for chroma key.', nargs=3, required=False, default=[0, 255, 0]);
 args = parser.parse_args()
 
 
@@ -50,7 +51,7 @@ end_loop = False;
 
 kernel1 = cv.getStructuringElement(cv.MORPH_ELLIPSE,(3,3));
 kernel2 = cv.getStructuringElement(cv.MORPH_ELLIPSE,(3,3));
-green_image = None;
+background_colour_image = None;
 next_frame_id = 0;
 
 def addFrame(capture):
@@ -85,6 +86,15 @@ cropping = False;
 update_ROI = False;
 ROI_background = None;
 
+background_colour = args.colour;
+
+if not isinstance(args.ROI, NoneType):
+    ROI_corners = [(args.ROI[0], args.ROI[1]), (args.ROI[2], args.ROI[3])];
+    cropping = False;
+    update_ROI = True;
+
+
+
 while not end_loop:
 
     addFrame(capture);
@@ -93,16 +103,13 @@ while not end_loop:
         end_loop = True;
     else:
 
-        if isinstance(green_image, NoneType):
+        if isinstance(background_colour_image, NoneType):
             cv.namedWindow("frame", cv.WINDOW_AUTOSIZE);
             cv.setMouseCallback("frame", clickAndCrop);
             cv.createTrackbar('Threshlod', 'frame', round(255 / 3.5), 255, nothing);
-            # cv.createTrackbar('Threshlod R', 'frame', round(255 / 3.5), 255, nothing);
-            # cv.createTrackbar('Threshlod G', 'frame', round(255 / 3.5), 255, nothing);
-            # cv.createTrackbar('Threshlod B', 'frame', round(255 / 3.5), 255, nothing);
 
-            green_image = np.zeros((frame.shape[0], frame.shape[1], 3), np.float);
-            green_image[:] = (0, 1,0);
+            background_colour_image = np.zeros((frame.shape[0], frame.shape[1], 3), np.float);
+            background_colour_image[:] = (background_colour[2] / 255, background_colour[1] / 255, background_colour[0] / 255);
 
         if len(ROI_corners) == 2:
             ROI_frame = copy.deepcopy(frame[ROI_corners[0][1]:ROI_corners[1][1], ROI_corners[0][0]:ROI_corners[1][0]]);
@@ -117,12 +124,11 @@ while not end_loop:
             if cropping == False and ROI_frame.shape[0] > 0 and ROI_frame.shape[1] > 0:
                 if update_ROI or ROI_background.shape[0] != ROI_frame.shape[0] or ROI_background.shape[1] != ROI_frame.shape[1]:
                     ROI_background = copy.deepcopy(background[ROI_corners[0][1]:ROI_corners[1][1], ROI_corners[0][0]:ROI_corners[1][0]]);
-                    cv.imshow('ROI_background', ROI_background);
 
                     update_ROI = False;
 
-                    green_image = np.zeros((ROI_frame.shape[0], ROI_frame.shape[1], 3), np.float);
-                    green_image[:] = (0, 1,0);
+                    background_colour_image = np.zeros((ROI_frame.shape[0], ROI_frame.shape[1], 3), np.float);
+                    background_colour_image[:] = (background_colour[2] / 255, background_colour[1] / 255, background_colour[0] / 255);
 
                 if ROI_background.shape[0] == ROI_frame.shape[0] and ROI_background.shape[1] == ROI_frame.shape[1]:
 
@@ -131,71 +137,20 @@ while not end_loop:
                     grey = cv.normalize(diff, np.zeros(diff.shape), 0, 255, cv.NORM_MINMAX);
                     grey = cv.cvtColor(grey, cv.COLOR_BGR2GRAY);
 
-                    # grey_r= cv.normalize(diff_r, np.zeros(diff_r.shape), 0, 255, cv.NORM_MINMAX).astype('uint8');
-                    # # grey_r = cv.cvtColor(grey_r, cv.COLOR_BGR2GRAY);
-                    #
-                    # grey_g= cv.normalize(diff_g, np.zeros(diff_g.shape), 0, 255, cv.NORM_MINMAX).astype('uint8');
-                    # # grey_g = cv.cvtColor(grey_g, cv.COLOR_BGR2GRAY);
-                    #
-                    # grey_b= cv.normalize(diff_b, np.zeros(diff_b.shape), 0, 255, cv.NORM_MINMAX).astype('uint8');
-                    # # grey_b = cv.cvtColor(grey_b, cv.COLOR_BGR2GRAY);
-
-                    #
-                    cv.imshow('diff', grey);
-                    # cv.imshow('diff_g', grey_g);
-                    # cv.imshow('diff_b', grey_b);
-                    #
-                    # cv.imshow('ROI_frame_r', ROI_frame_r);
-                    # cv.imshow('ROI_frame_g', ROI_frame_g);
-                    # cv.imshow('ROI_frame_b', ROI_frame_b);
 
                     ret, foreground_mask = cv.threshold(grey, cv.getTrackbarPos('Threshlod','frame'), 255, cv.THRESH_BINARY);
-                    # ret, foreground_mask_r = cv.threshold(grey_r, cv.getTrackbarPos('Threshlod R','frame'), 255, cv.THRESH_BINARY);
-                    # ret, foreground_mask_g = cv.threshold(grey_g, cv.getTrackbarPos('Threshlod G','frame'), 255, cv.THRESH_BINARY);
-                    # ret, foreground_mask_b = cv.threshold(grey_b, cv.getTrackbarPos('Threshlod B','frame'), 255, cv.THRESH_BINARY);
-                    cv.imshow('grey', grey);
-                    cv.imshow('foreground_mask', foreground_mask);
-                    # cv.imshow('foreground_mask_r', foreground_mask_r);
-                    # cv.imshow('foreground_mask_g', foreground_mask_g);
-                    # cv.imshow('foreground_mask_b', foreground_mask_b);
-                    # cv.imshow('foreground_mask', foreground_mask);
 
                     foreground_mask = cv.medianBlur(foreground_mask, 1);
-                    cv.imshow('medianBlur', foreground_mask);
                     foreground_mask = cv.dilate(foreground_mask, kernel2, iterations = 3);
-                    cv.imshow('dilate', foreground_mask);
                     foreground_mask = cv.morphologyEx(foreground_mask, cv.MORPH_CLOSE, kernel1);
-                    cv.imshow('MORPH_CLOSE', foreground_mask);
                     foreground_mask = cv.morphologyEx(foreground_mask, cv.MORPH_OPEN, kernel2);
-                    cv.imshow('MORPH_OPEN', foreground_mask);
                     foreground_mask = foreground_mask.astype('float') / 255;
 
-
-                    # foreground_mask_r = cv.dilate(foreground_mask_r, kernel2, iterations = 3);
-                    # foreground_mask_r = cv.morphologyEx(foreground_mask_r, cv.MORPH_CLOSE, kernel1);
-                    # foreground_mask_r = cv.morphologyEx(foreground_mask_r, cv.MORPH_OPEN, kernel2);
-                    #
-                    # foreground_mask_g = cv.dilate(foreground_mask_g, kernel2, iterations = 3);
-                    # foreground_mask_g = cv.morphologyEx(foreground_mask_g, cv.MORPH_CLOSE, kernel1);
-                    # foreground_mask_g = cv.morphologyEx(foreground_mask_g, cv.MORPH_OPEN, kernel2);
-                    #
-                    # foreground_mask_b = cv.dilate(foreground_mask_b, kernel2, iterations = 3);
-                    # foreground_mask_b = cv.morphologyEx(foreground_mask_b, cv.MORPH_CLOSE, kernel1);
-                    # foreground_mask_b = cv.morphologyEx(foreground_mask_b, cv.MORPH_OPEN, kernel2);
-                    #
-                    # foreground_mask_r = cv.medianBlur(foreground_mask_r, 3);
-                    # foreground_mask_g = cv.medianBlur(foreground_mask_g, 3);
-                    # foreground_mask_b = cv.medianBlur(foreground_mask_b, 3);
 
                     alpha3 = cv.pyrUp(cv.pyrUp(np.stack([foreground_mask]*3, axis=2)));
                     alpha3 = alpha3[0:ROI_frame.shape[0], 0:ROI_frame.shape[1]];
 
-
-                    #
-                    # cv.imshow('foreground_mask', alpha3);
-                    # print(foreground_mask_g.shape, ROI_frame.shape)
-                    cv.imshow('ROI_frame', ROI_frame);
-                    cv.imshow('chroma key', alpha3 * ROI_frame / (255) + (1-alpha3) * green_image);
+                    cv.imshow('chroma key', alpha3 * ROI_frame / (255) + (1-alpha3) * background_colour_image);
 
         keyboard = cv.waitKey(30)
         if keyboard == 'b' or keyboard == ord('b'):
@@ -211,28 +166,16 @@ while not end_loop:
             background = background.astype('uint8');
 
             ROI_background = cv.GaussianBlur(background, (5,5),0);
-            cv.imshow('background', background);
-
-            # background_r, background_g, background_b = normaliseRGB(frame);
-            #
-            # ROI_background_r = cv.GaussianBlur(background_r, (5,5),0);
-            # ROI_background_g = cv.GaussianBlur(background_g, (5,5),0);
-            # ROI_background_b = cv.GaussianBlur(background_b, (5,5),0);
 
         elif keyboard == 'q' or keyboard == ord('q') or keyboard == 27:
             end_loop = True;
 
-        # draw a rectangle around the region of interest
+        # Draw a rectangle around the region of interest
         if len(ROI_corners) == 2:
             cv.rectangle(frame, ROI_corners[0], ROI_corners[1], (0, 255, 0), 2)
 
         cv.imshow("frame", frame)
 
-
-#
-# # if there are two reference points, then crop the region of interest
-# # from teh image and display it
-# if len(refPt) == 2:
-# 	roi = clone[refPt[0][1]:refPt[1][1], refPt[0][0]:refPt[1][0]]
-# 	cv2.imshow("ROI", roi)
-# 	cv2.waitKey(0)
+# Print the corners of the ROI
+if len(ROI_corners) == 2:
+    print("ROI:", ROI_corners);
